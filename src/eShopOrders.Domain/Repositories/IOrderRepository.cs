ba4bc365c603924588c628ed89292579cf0f361f
EvertonKluczkovski <kluczkovski@hotmail.com>
2024-07-07 17:29:26 +0100

    missing Repo

--- src/eShopOrders.Domain/Repositories/IOrderRepository.cs
+++ src/eShopOrders.Domain/Repositories/IOrderRepository.cs
@@ -1,5 +1,12 @@
+using eShopOrders.Domain.Entities;
+
 namespace eShopOrders.Domain.Repositories;
 
 public class IOrderRepository
 {
+    Task<Order> GetByAsync(Guid id)
+
+    Task AddAsync(Order order)
+
+    Task UpdateAsync(Order order)
 }
